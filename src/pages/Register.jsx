0bf78db70043399f5f1695d19c39e77acd16b1bc
YonnierDev <yonnierleon@gmail.com>
2025-03-28 17:49:40 -0500

    mejora para registrar usuarios

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { api } from "./api/api";
-import { useNavigate,Link } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom";
 import "./AuthForm.css"; // Asegúrate de importar el CSS
 import logo from "./logos.png"; // Ruta de la imagen
 const Register = () => {
@@ -10,93 +10,101 @@ const Register = () => {
   const [fecha_nacimiento, setFecha_nacimiento] = useState("");
   const [contrasena, setContrasena] = useState("");
   const [genero, setGenero] = useState("");
-  const [estado, setEstado] = useState("");
-  const [rolid, setRolid] = useState("");
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await api.post("/user", { nombre, correo, contrasena });
-      console.log("✅ Usuario registrado:", response.data);
+      const response = await api.post("/usuario", {
+        nombre,
+        apellido,
+        correo,
+        fecha_nacimiento,
+        contrasena,
+        genero,
+      });
+
+      console.log("Usuario registrado:", response.data);
+      alert("REGISTRADO EXITOSAMENTE!");
+
       navigate("/usuarios");
     } catch (error) {
-      alert("Error en la petición, revisa la consola.");
+      if (error.response) {
+        if (error.response.status === 400) {
+          alert(error.response.data.mensaje || "El correo ya está registrado.");
+        } else {
+          alert("Error en la petición, revisa la consola.");
+        }
+      } else {
+        alert("Error en la conexión con el servidor.");
+      }
       console.error("ERROR AL REGISTRAR:", error);
     }
   };
 
   return (
     <div className="auth-container">
-           
-          <div className="auth-box">
-            <div className="title">
-              <h2>Registro</h2>
-            </div>
-              <form onSubmit={handleRegister}>
-        <input
-          type="text"
-          placeholder="Nombre"
-          onChange={(e) => setNombre(e.target.value)}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Apellido"
-          onChange={(e) => setApellido(e.target.value)}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Correo"
-          onChange={(e) => setCorreo(e.target.value)}
-          required
-        />
-        <input
-          type="text"
-          placeholder="fecha de nacimiento"
-          onChange={(e) => setFecha_nacimiento(e.target.value)}
-          required
-        />
+      <div className="auth-box">
+        <div className="title">
+          <h2>Registro</h2>
+        </div>
+        <form onSubmit={handleRegister}>
           <input
             type="text"
-          placeholder="Contraseña"
-          onChange={(e) => setContrasena(e.target.value)}
-          required
-        />
+            placeholder="Nombre"
+            onChange={(e) => setNombre(e.target.value)}
+            required
+          />
           <input
             type="text"
-          placeholder="Genero"
-          onChange={(e) => setGenero(e.target.value)}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Estado"
-          onChange={(e) => setEstado(e.target.value)}
-          required
-        />
-        <button type="submit">Registrarse</button>
-      </form>
-             <div className="separator">
-          <span>----------------------------- o ----------------------------</span>
+            placeholder="Apellido"
+            onChange={(e) => setApellido(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Correo"
+            onChange={(e) => setCorreo(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="fecha de nacimiento"
+            onChange={(e) => setFecha_nacimiento(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            onChange={(e) => setContrasena(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Genero"
+            onChange={(e) => setGenero(e.target.value)}
+            required
+          />
+          <button type="submit">Registrarse</button>
+        </form>
+        <div className="separator">
+          <span>
+            ----------------------------- o ----------------------------
+          </span>
         </div>
 
-        <div  className="title">
-        <p className="register-text">
-          ¿Tienes cuenta? <Link to="/login">Login</Link>
+        <div className="title">
+          <p className="register-text">
+            ¿Tienes cuenta? <Link to="/login">Login</Link>
           </p>
         </div>
       </div>
-       <div className="logo">
-            <img src={logo} alt="Photobella Logo" className="logo-img" />
-            <div className="logo-text">
-            </div>
-        </div>
+      <div className="logo">
+        <img src={logo} alt="Photobella Logo" className="logo-img" />
+        <div className="logo-text"></div>
+      </div>
     </div>
-
   );
 };
 
 export default Register;
-
