98e0011a91cf9cf00f0d21cb264785852a7ffc5c
camilofive <florescamilo@gmail.com>
2025-04-07 20:13:06 -0500

    feat: cambios en Login.jsx

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://backend-1ky982i25-yonnierdevs-projects.vercel.app/api",
+  baseURL: "https://backend-1ky982i25-yonnierdevs-papprojects.vercel./api",
   headers: {
     "Content-Type": "application/json",
   },
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = ({ setIsAuthenticated }) => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("https://popnocturna.vercel.app/api/login", {
+      const response = await axios.post("https://backend-1ky982i25-yonnierdevs-projects.vercel.app/api/login", {
         correo,
         contrasena,
       });
