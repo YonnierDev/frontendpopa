71e2b78d5b03a525c0fd6c4aeaff34f54c10d398
camilofive <florescamilo@gmail.com>
2025-06-15 11:45:51 -0500

    fix: Actualizar configuración de Vercel y Vite para manejo de rutas

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Configuración para manejar rutas en producción
-  base: '/', // Asegura que las rutas sean relativas a la raíz
+  base: '/',
   server: {
-    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    strictPort: true,
+    historyApiFallback: {
+      disableDotRule: true,
+    },
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
-    // Configuración para manejar rutas en producción
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
       output: {
         manualChunks: {
           react: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['axios', 'react-toastify', 'react-icons'],
         },
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
   },
 })
